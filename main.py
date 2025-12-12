3276d2d6bda563bd4d68418e970401d200c38873
florrocha-azu <flor.rocha@est.iudigital.edu.co>
2025-12-12 15:27:09 -0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,18 @@
+from mini_turtle import adelante, abajo, reiniciar
+
+# Uso de la mini_tortuga
+reiniciar()
+adelante(7)
+abajo(3)
+
+reiniciar()
+
+adelante(5)
+abajo(5)
+
+reiniciar()
+
+adelante(3)
+abajo(7)
+
+reiniciar()
